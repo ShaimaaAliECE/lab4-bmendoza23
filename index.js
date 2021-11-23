aac92ff62de18341f52fd93cb155b12a35bbc788
bmendoza23 <bmendoz2@uwo.ca>
2021-11-23 18:29:32 -0500

    changes

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-//importing questions json file
-var questions = require('./questions.json');
+const questions = require('./questions.json');      //importing questions json file
+const express = require('express');
 
+const app = express();
+
+//Serve static content
+app.use(express.static('static'));
+
+app.use(express.urlencoded({
+        extended: true
+    }));
+
+//Handling quiz page
+app.post('/quiz', (req,res) => {
+    
+});
+
+app.listen(2000);   //Currently listening on port 2000
